28fed222b38474d327ffb387740eda1845f4d010
paulnien <paul-nienhaus@gmx.net>
2020-06-23 15:13:05 +0200

    23.06.2020 Links drin

--- app/src/main/java/com/example/gruppe3/Links.java
+++
@@ -1,6 +0,0 @@
-package com.example.gruppe3;
-
-public class Links
-{
-
-}
--- app/src/main/java/com/example/gruppe3/MainActivity.java
+++ app/src/main/java/com/example/gruppe3/MainActivity.java
@@ -89,4 +89,13 @@ public class MainActivity extends AppCompatActivity
 
         //return super.onOptionsItemSelected(item);
     }
+
+    public void openMensa(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.akafoe.de/gastronomie/speiseplaene-der-mensen/wh-bocholt/"));
+        startActivity(browserIntent);
+    }
+    public void openStundenplan(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://splan.w-hs.de/splan/mobile?lan=de&acc=true&act=tt&sel=pg&pu=55&og=24&pg=WINFO%202&sd=true&dfc=2020-06-23&loc=5&sa=false&cb=o"));
+        startActivity(browserIntent);
+    }
 }
